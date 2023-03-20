88e4e372dfaff4f176f73a7e710f859dd28cb393
GuilhermeBottolo <128073903+GuilhermeBottolo@users.noreply.github.com>
2023-03-20 12:39:27 -0300

    v3

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function dados(){
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$',  '&', '*'
       ];
-
-    let quantidade = 10;
+    let user = document.getElementById('A').value;
+ 
+    let quantidade = user;
     let escolhidos = [];
     
     
@@ -17,8 +18,9 @@ function dados(){
       }
          
     
-    
+    console.log(user)
     let titulo2 = document.getElementById('text');
     titulo2.innerHTML = (escolhidos.join('')) 
 }
    
+
